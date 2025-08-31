71808fcf5e46c4e63f347326256e72d25288609e
Филя Матроскин <filyamatross@gmail.com>
2025-08-31 22:08:34 +0300

    Initial commit

---
+++ AuthenticationWindow/Auth.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AuthenticationWindow;
+
+public partial class Auth : UserControl{
+    
+    
+    
+    public Auth() {
+        InitializeComponent();
+    }
+}
